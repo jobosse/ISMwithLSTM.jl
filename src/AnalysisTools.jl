a2f0f4463dee99b38ea1bdc0f6960a411ed47423
johannesbosse <johannes.bosse@rwth-aachen.de>
2022-08-15 15:27:51 +0200

    First docs

--- src/AnalysisTools.jl
+++ src/AnalysisTools.jl
@@ -1,10 +1,17 @@
-using Pandas    
+using CSV  
 
 """
-Load in raw ΔTT data
+Load in raw ΔTT data using CSV.jl package
 
+# Arguments
+
+* 'data_path::String'
+
+
+# Returns
+* CSV.File with columns : ["ΔTT", "yr", "day", "T_n","T_s"]
 """
-
 function LoadData(data_path::String)
-    a = (4,5)
+    data = CSV.File(data_path, header=["ΔTT", "yr", "day", "T_n","T_s"])
+    return data
 end
