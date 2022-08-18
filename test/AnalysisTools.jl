5731cfdc1d104a273e961db74c1d4449a54e6314
johannesbosse <johannes.bosse@rwth-aachen.de>
2022-08-18 12:50:27 +0200

    Update

--- test/AnalysisTools.jl
+++ test/AnalysisTools.jl
@@ -1,8 +1,8 @@
 #include("../src/LSTM.jl")
 
 @testset "AnalysisTools.jl" begin
-    test_lstm = loadLSTM("TestData/test_lstm.bson")
-    test_data_path = "TestData/deltaTT.txt"
-    @test RunLSTM(test_lstm, [test_data_path],(1948,1949))[end,2] ≈ 0.318445 atol=0.0001
+    #test_lstm = loadLSTM("TestData/test_lstm.bson")
+    #test_data_path = "TestData/deltaTT.txt"
+    #@test RunLSTM(test_lstm, [test_data_path],(1948,1949))[end,2] ≈ 0.318445 atol=0.0001
     
 end
